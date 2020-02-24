8a50b30305e9e852b8fffd65a194e926f6c61b1a
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-24 17:21:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,4 +21,16 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array) 
-  
+  index = 0 
+  ans = []
+  while index < source_array.length do 
+    temp = source_array[index] * 2 
+    ans.push(temp)
+    index += 1 
+  end
+  ans
+end
+
+def 
+    
+    
