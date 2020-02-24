846bb0a9dceff5b2bd216e7a9c25dfa29bce927a
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-24 17:19:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negativize(source_array)
+  index = 0 
+  ans = []
+  while index < source_array.length do 
+    temp = source_array[index] * -1
+    ans.push(temp)
+    index += 1 
+  end
+  ans
+end
