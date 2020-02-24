ba7e5bc196685c42927f0914c2902ea07874be1b
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-24 17:28:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,6 +62,15 @@ def reduce_to_all_true(source_array)
   return true 
 end
 
-
+def reduce_to_any_true(source_array) 
+  index = 0 
+  while index < source_array.length do 
+    if source_array[index]
+      return true 
+    end
+    index += 1 
+  end
+  return false 
+end
     
     
