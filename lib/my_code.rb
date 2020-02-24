b168abc7f347cda8efb69cfc867554325c4b1bc1
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-24 17:22:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,6 +31,17 @@ def map_to_double(source_array)
   ans
 end
 
-def 
+def map_to_square(source_array) 
+  index = 0 
+  ans = []
+  while index < source_array.length do 
+    temp = source_array[index] ** 2 
+    ans.push(temp)
+    index += 1 
+  end
+  ans
+end
+
+
     
     
